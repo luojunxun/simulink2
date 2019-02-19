4989a6aac4b95eced11d6748d50ae9f22089d9f0
mluo <mluo@SE-LOANER-05.dhcp.mathworks.com>
2019-02-19 13:00:04 +0100

    aa

--- runVerification2.m
+++ runVerification2.m
@@ -23,16 +23,16 @@ load_system(myModel);
 % set_param(gcs,'SimulationCommand','Update');
 set_param(bdroot,'SimulationCommand','Update');
 
-% %% Execute model checks
-% fprintf(2,'*** Execute model checks ***\n');
-% % Create list of checks and models to run.
-% %CheckIDList ={'mathworks.maab.jc_0021', 'mathworks.iec61508.RootLevelInports'};
-% SysList={myModel};
-% % Run the Model Advisor.
-% checkResult = ModelAdvisor.run(SysList,'Configuration','ModelAdvisorChecksFilter.mat');
-% fprintf(2,'Result: Number of checks failed: %i\n', checkResult{1}.numFail);
-% fprintf(2,'        Number of checks with warnings: %i\n', checkResult{1}.numWarn);
-% fprintf(2,'        Number of checks passed: %i\n', checkResult{1}.numPass);
+%% Execute model checks
+fprintf(2,'*** Execute model checks ***\n');
+% Create list of checks and models to run.
+%CheckIDList ={'mathworks.maab.jc_0021', 'mathworks.iec61508.RootLevelInports'};
+SysList={myModel};
+% Run the Model Advisor.
+checkResult = ModelAdvisor.run(SysList,'Configuration','ModelAdvisorChecksFilter.mat');
+fprintf(2,'Result: Number of checks failed: %i\n', checkResult{1}.numFail);
+fprintf(2,'        Number of checks with warnings: %i\n', checkResult{1}.numWarn);
+fprintf(2,'        Number of checks passed: %i\n', checkResult{1}.numPass);
 
 %% Run tests
 fprintf(2,'*** Execute test cases ***\n');
@@ -103,9 +103,9 @@ fprintf(2,'*** Result from test ***\n');
 fprintf(2,fileread(tapFile));
 fprintf(2,'\n');
 
-% %% Generate code
-% fprintf(2,'*** Generate Code ***\n');
-% rtwbuild(myModel);
+%% Generate code
+fprintf(2,'*** Generate Code ***\n');
+rtwbuild(myModel);
 % 
 % %% Analyze code
 % fprintf(2,'*** Start Polyspace Code Prover ***\n');
